34272be96afd039c2325574916e3e680d1dc6b87
Nicolas <nicolas@korbit.com>
2019-01-10 11:06:38 -0800

    pylint

--- main.py
+++ main.py
@@ -7,53 +7,53 @@ from os import system
 import sys
 import logging
 import argparse
-from pymemcache.client import Client
 import sqlite3
+from pymemcache.client import Client
 
 # globals
-log = logging.getLogger()
-log.setLevel(logging.INFO)
-description = 'TenantBase -- sqlite+memcached service.'
-serve_help = 'starts database.sqlite.'
-show_help = 'displays all stored key-value pairs.'
-host = 'localhost'
-port = 11211
-start_memcached = 'memcached -d -vv'
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+DESCRIPTION = 'TenantBase -- sqlite+memcached service.'
+SERVE_HELP = 'starts database.sqlite.'
+SHOW_HELP = 'displays all stored key-value pairs.'
+HOST = 'localHOST'
+PORT = 11211
+START_MEMCACHED = 'memcached -d -vv'
 
 # functions
 def run_command(command):
     """Wrapper around os.system, will raise exception if command did not exit cleanly"""
     exit_code = system(command)
-    if exit_code is not 0:
+    if exit_code != 0:
         raise Exception('Cannot execute command: %s. Did you install it?' % command)
 
 def parse_args(args):
     """parses command line arguments"""
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('-s', '--serve', type=str, dest='serve',
-                        help=serve_help)
+                        help=SERVE_HELP)
     parser.add_argument('-sh', '--show', type=str, dest='show',
-                            help=show_help)
+                        help=SHOW_HELP)
 
     return parser.parse_args(args)
 
 # main
 def main(argv=None):
     """main function"""
-    log.info('tenant-base main starting')
+    LOG.info('tenant-base main starting')
 
     parser = parse_args(argv)
 
     if parser.serve:
-        log.info('Starting memcached server')
+        LOG.info('Starting memcached server')
         print('Starting memcached server')
         try:
-            run_command(start_memcached)
-            log.info('Memcached server listening on: %s:%s', host, str(port))
-            print('Memcached server listening on: {}:{}'.format(host, port))
+            run_command(START_MEMCACHED)
+            LOG.info('Memcached server listening on: %s:%s', HOST, str(PORT))
+            print('Memcached server listening on: {}:{}'.format(HOST, PORT))
 
         except (Exception, ConnectionRefusedError) as error:
-            log.error('Error: %s', error)
+            LOG.error('Error: %s', error)
             sys.exit(1)
 
     elif parser.show:
