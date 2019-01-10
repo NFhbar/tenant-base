0472d4523081647c51bdedf06968d289a796d31d
Nicolas <nicolas@korbit.com>
2019-01-10 11:01:16 -0800

    initial memcached implementation

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Tenant Base Backend
 
 [![CircleCI](https://circleci.com/gh/NFhbar/tenant-base.svg?style=svg)](https://circleci.com/gh/NFhbar/tenant-base)
-
-[![Coverage Status](https://coveralls.io/repos/github/NFhbar/tenant-base/badge.svg?branch=issue_1)](https://coveralls.io/github/NFhbar/tenant-base?branch=issue_1)
+[![Coverage Status](https://coveralls.io/repos/github/NFhbar/tenant-base/badge.svg?branch=master)](https://coveralls.io/github/NFhbar/tenant-base?branch=master)
 
 Simple key-value storage which implements a small subset of the [memcached protocol](https://memcached.org/).
 Data is persisted through [SQLite](https://www.sqlite.org/index.html).
 
 ## Requirements
+- [Memcached](https://memcached.org/)
+- [Telnet](https://wincent.com/wiki/Testing_memcached_with_telnet)
 - Python 3.6
 - Pip
 
@@ -40,7 +41,9 @@ To run the unit tests:
 (tenant-base) $ python3 -m pytest --cov-report term-missing --cov=main tests/ -s
 ```
 ## Pylint
-`Pylint` is configured through `.pylintrc`. To run:
+`Pylint` is configured through `.pylintrc`.
+
+To run:
 ```
 (tenant-base) $ pylint main.py
 ```
@@ -49,6 +52,9 @@ To run `pylint` on the entire project:
 ```
 $ find . -iname "*.py" | xargs pylint
 ```
+
+## Memcahed
+
 
 ## Contact
 Nicolas Frega
--- main.py
+++ main.py
@@ -1,21 +1,68 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 TenantBase Backend
 Key-value storage server implemented in SQlite and memmcached.
-
-
 """
+from os import system
+import sys
 import logging
+import argparse
+from pymemcache.client import Client
+import sqlite3
 
-__author__ = 'Nicolas Ferga'
-__version__ = "0.1.0"
+# globals
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+description = 'TenantBase -- sqlite+memcached service.'
+serve_help = 'starts database.sqlite.'
+show_help = 'displays all stored key-value pairs.'
+host = 'localhost'
+port = 11211
+start_memcached = 'memcached -d -vv'
 
-LOG = logging.getLogger()
-LOG.setLevel(logging.INFO)
+# functions
+def run_command(command):
+    """Wrapper around os.system, will raise exception if command did not exit cleanly"""
+    exit_code = system(command)
+    if exit_code is not 0:
+        raise Exception('Cannot execute command: %s. Did you install it?' % command)
 
-def main():
+def parse_args(args):
+    """parses command line arguments"""
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-s', '--serve', type=str, dest='serve',
+                        help=serve_help)
+    parser.add_argument('-sh', '--show', type=str, dest='show',
+                            help=show_help)
+
+    return parser.parse_args(args)
+
+# main
+def main(argv=None):
     """main function"""
-    LOG.info('tenant-base')
+    log.info('tenant-base main starting')
+
+    parser = parse_args(argv)
+
+    if parser.serve:
+        log.info('Starting memcached server')
+        print('Starting memcached server')
+        try:
+            run_command(start_memcached)
+            log.info('Memcached server listening on: %s:%s', host, str(port))
+            print('Memcached server listening on: {}:{}'.format(host, port))
+
+        except (Exception, ConnectionRefusedError) as error:
+            log.error('Error: %s', error)
+            sys.exit(1)
+
+    elif parser.show:
+        print('showing')
+
+    else:
+        print('Run python main.py -h for help')
+
+
 
 if __name__ == '__main__':
     main()
